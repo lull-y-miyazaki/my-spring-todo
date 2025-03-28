b687a8eefb3f601d538cfbcd63e40fbfe84d0b9b
lull-y-miyazaki <y.miyazaki@lull-inc.co.jp>
2025-03-28 20:20:41 +0900

    ユーザー新規登録画面の表示・新規登録処理の実装

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,5 +1,53 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+
+@Controller
 public class UserController {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	// 新規ユーザー登録画面の表示
+	@GetMapping("/users/add")
+	public String create() {
+
+		return "user/addUser";
+	}
+
+	// 新規登録の処理内容
+	@PostMapping("/users/add")
+	public String store(
+			@RequestParam String name,
+			@RequestParam String email,
+			@RequestParam String password,
+			@RequestParam String password_confirm,
+			Model model) {
+
+		User user = new User(name, email, password);
+		userRepository.save(user);
+
+		// 登録するユーザーの確認用
+		System.out.println("登録データ: " + user.toString());
+
+		return "redirect:/login";
+	}
+
+	// ログイン画面の表示
+	@GetMapping({ "/", "/login" })
+	public String index(
+			@RequestParam(defaultValue = "") String error,
+			Model model) {
+
+		return "user/login";
+	}
 
 }
