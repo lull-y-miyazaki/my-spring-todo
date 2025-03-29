c7408357b19785a0df71c7cfc03b3f756abedbdb
lull-y-miyazaki <y.miyazaki@lull-inc.co.jp>
2025-03-29 20:12:55 +0900

    Repositoryをinterfaceに修正、Task絞り込み用のクエリメソッドの追加

--- src/main/java/com/example/demo/repository/CategoryRepository.java
+++ src/main/java/com/example/demo/repository/CategoryRepository.java
@@ -1,5 +1,10 @@
 package com.example.demo.repository;
 
-public class CategoryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.Category;
+
+//JpaRepository は Spring Data JPA によって提供される データベース操作（CRUD）を簡単に実装できるインターフェース
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
 }
