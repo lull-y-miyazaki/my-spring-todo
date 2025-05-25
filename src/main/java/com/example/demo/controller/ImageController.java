ae0168b3a1af44f00e70c104662a59c148be2035
lull-y-miyazaki <y.miyazaki@lull-inc.co.jp>
2025-05-25 14:30:10 +0900

    画像をアップロードできるように修正

---
+++ src/main/java/com/example/demo/controller/ImageController.java
@@ -0,0 +1,47 @@
+package com.example.demo.controller;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class ImageController {
+
+	@GetMapping("/image")
+	public String show() {
+		return "image";
+	}
+
+	// 画像アップロード & 表示
+	@PostMapping("/upload")
+	public String uploadImage(
+			@RequestParam("file") MultipartFile file,
+			Model model) {
+		try {
+			// ファイル名（別名を付けても良い）
+			String filename = file.getOriginalFilename();
+			// 保存先パス
+			String filePath = "static/image/" + filename;
+			// ファイルをバイナリデータとして取得
+			byte[] content = file.getBytes();
+			// 保存
+			Files.write(Paths.get(filePath), content);
+
+			// アップロードした画像のURLを画面に渡す
+			String imageUrl = "/image/" + filename;
+			model.addAttribute("imageUrl", imageUrl);
+		} catch (Exception e) {
+			// エラー時
+			e.printStackTrace();
+		}
+
+		// image.htmlの表示
+		return "image";
+	}
+}
