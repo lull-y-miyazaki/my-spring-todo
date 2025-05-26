b340e7982fed5878496e4672bfc92f9c633b6509
lull-y-miyazaki <y.miyazaki@lull-inc.co.jp>
2025-05-26 20:11:02 +0900

    ユーザーとタスクに画像を持たせられるようにDBを変更

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,30 +3,18 @@ INSERT INTO categories(name) VALUES('仕事');
 INSERT INTO categories(name) VALUES('個人');
 INSERT INTO categories(name) VALUES('その他');
 
--- users テーブルにデータを挿入するクエリ
---INSERT INTO users (id, email, name, password)
---VALUES
---(1, 'tanaka@aaa.com', '田中太郎', 'test123'),
---(2, 'suzuki@aaa.com', '鈴木一郎', 'test456'),
---(3, 'test@com', 'テストユーザー', 'testuser');
-
 -- 上記のようにidを指定してデータを挿入すると、idが重複エラーとなるため下記に修正
-INSERT INTO users (email, name, password) VALUES
-('tanaka@aaa.com', '田中太郎', 'test123'),
-('suzuki@aaa.com', '鈴木一郎', 'test456'),
-('testuser@com', 'テストユーザー', 'testuser'),
-('test@com', 'テスト', 'testuser');
+INSERT INTO users (email, name, password, icon) VALUES
+('tanaka@aaa.com', '田中太郎', 'test123', 'user_icon.png'),
+('suzuki@aaa.com', '鈴木一郎', 'test456', 'user_icon.png'),
+('testuser@com', 'テストユーザー', 'testuser', 'user_icon.png'),
+('test@com', 'テスト', 'testuser', 'user_icon.png');
 
--- tasks テーブルにデータを挿入するクエリ
---INSERT INTO tasks (id, category_id, user_id, title, closing_date, progress, memo)
---VALUES
---(1, 1, 1, '見積もり', '2025-12-31', 0, '案件に適した見積もりを取る');
-
-INSERT INTO tasks (category_id, user_id, title, closing_date, progress, memo)
+INSERT INTO tasks (category_id, user_id, title, closing_date, progress, memo, image)
 VALUES
-(1, 1, '見積もり', '2025-12-31', 0, '案件に適した見積もりを取る'),
-(2, 1, 'ゲームの作り方', '2025-12-31', 1, 'ゲームの作り方を明日までに考える'),
-(3, 1, 'コーヒー豆の煎り方', '2025-12-31', 2, 'コーヒー豆の煎り方を明日までにマスターする'),
-(1, 2, '請求', '2025-12-31', 0, '請求金額を明日までに提出'),
-(2, 2, '漫画の描き方', '2025-12-31', 1, '漫画の描き方を明日までにマスターする'),
-(3, 3, 'リマインド', '2025-12-31', 2, '明日までにリマインドをする');
+(1, 1, '見積もり', '2025-12-31', 0, '案件に適した見積もりを取る', 'task_icon.png'),
+(2, 1, 'ゲームの作り方', '2025-12-31', 1, 'ゲームの作り方を明日までに考える', 'task_image.jpeg'),
+(3, 1, 'コーヒー豆の煎り方', '2025-12-31', 2, 'コーヒー豆の煎り方を明日までにマスターする', 'task_icon.png'),
+(1, 2, '請求', '2025-12-31', 0, '請求金額を明日までに提出', 'task_image.jpeg'),
+(2, 2, '漫画の描き方', '2025-12-31', 1, '漫画の描き方を明日までにマスターする', 'task_icon.png'),
+(3, 3, 'リマインド', '2025-12-31', 2, '明日までにリマインドをする', 'task_image.jpeg');
